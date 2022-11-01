bcd59198ca5c34d5a009bf3e07324f8bac570bdb
lgq0673 <3258956344@qq.com>
2022-11-01 10:20:38 +0800

    第一次提交

---
+++ git_demo_test01/src/main/java/com/lgq/git/test/Hello.java
@@ -0,0 +1,11 @@
+package com.lgq.git.test;
+
+/**
+ * @Author: Li Guo Qiang
+ * @Date: 2022/11/1 10:11
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
